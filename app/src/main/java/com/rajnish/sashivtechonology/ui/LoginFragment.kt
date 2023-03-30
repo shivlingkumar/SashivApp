504dc44d75813e0dbef8953ba4ceefb4f0a8d69a
Rja17 <kumarrajnish.stm@gmail.com>
2023-03-30 16:24:29 +0530

    login page work done

---
+++ app/src/main/java/com/rajnish/sashivtechonology/ui/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.rajnish.sashivtechonology.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rajnish.sashivtechonology.R
+
+class LoginFragment : Fragment(R.layout.fragment_login){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
